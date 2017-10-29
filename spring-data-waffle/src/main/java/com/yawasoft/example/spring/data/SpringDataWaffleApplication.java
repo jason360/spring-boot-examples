fc637c11d81765edff79c88cca4d54ff6b71526c
jason360 <jason.yd.wu@gmail.com>
2017-10-29 15:19:21 -0400

    initial check in

---
+++ spring-data-waffle/src/main/java/com/yawasoft/example/spring/data/SpringDataWaffleApplication.java
@@ -0,0 +1,12 @@
+package com.yawasoft.example.spring.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataWaffleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataWaffleApplication.class, args);
+	}
+}
